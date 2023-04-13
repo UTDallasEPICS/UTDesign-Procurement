5371e79f6d3d01be11f3dd425bf7c4b8b87f8106
bryceabraham <121129581+bryceabraham@users.noreply.github.com>
2023-04-13 20:25:04 +0000

    changed buttons

--- procurement-manager/pages/orders/RequestCard.tsx
+++ procurement-manager/pages/orders/RequestCard.tsx
@@ -2,45 +2,57 @@
 import React from 'react';
 import { Row, Col, Card, Button } from 'react-bootstrap';
 
-const RequestCard = () => {
+const RequestCard: React.FC = () => {
   return (
-    <Card style={{ backgroundColor: '#f8f9fa' }}>
-      <Card.Body>
-        <Row>
-          <Col>
-            <h4>Request Number</h4>
-            <h5>#1</h5>
-          </Col>
-          <Col>
-            <h4>Date Requested</h4>
-            <h5>4/11/2023</h5>
-          </Col>
-          <Col>
-            <h4>Date Needed</h4>
-            <h5>4/13/2023</h5>
-          </Col>
-          <Col>
-            <h5>Order Total: $200</h5>
-            <h5>Budget Used: $300/500</h5>
-          </Col>
-        </Row>
-        <Row className="d-flex justify-content-between">
-          <Col md="auto">
-            <a href="#" className="text-info" style={{ textDecoration: 'underline' }}>
-              View Request Form
-            </a>
-          </Col>
-          <Col md="auto">
-            <Button variant="dark" size="lg" className="mr-2" style={{ minWidth: '100px' }}>
-              REJECT
-            </Button>
-            <Button variant="success" size="lg" style={{ minWidth: '100px' }}>
-              APPROVE
-            </Button>
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+    <Row className="mb-4">
+      <Col>
+        <Card style={{ backgroundColor: '#f8f9fa' }}>
+          <Card.Body>
+            <Row className="smaller-row">
+              <Col>
+                <h4>Request Number</h4>
+                <h5>#1</h5>
+              </Col>
+              <Col>
+                <h4>Date Requested</h4>
+                <h5>4/11/2023</h5>
+              </Col>
+              <Col>
+                <h4>Date Needed</h4>
+                <h5>4/13/2023</h5>
+              </Col>
+              <Col>
+                <h5>Order Total: $200</h5>
+                <h5>Budget Used: $300/500</h5>
+              </Col>
+            </Row>
+            <Row className="smaller-row d-flex justify-content-between">
+              <Col md="auto">
+                <a href="#" className="text-info" style={{ textDecoration: 'underline' }}>
+                  View Request Form
+                </a>
+              </Col>
+              <Col md="auto">
+                <Button
+                  variant="dark"
+                  size="lg"
+                  style={{ minWidth: '300px', marginRight: '20px' }}
+                >
+                  REJECT
+                </Button>{' '}
+                <Button
+                  variant="success"
+                  size="lg"
+                  style={{ minWidth: '300px' }}
+                >
+                  APPROVE
+                </Button>{' '}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
