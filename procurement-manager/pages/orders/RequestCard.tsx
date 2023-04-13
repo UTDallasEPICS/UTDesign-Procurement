0612f0172675f01d62e4637bedfc4b8b4f50ae9f
bryceabraham <121129581+bryceabraham@users.noreply.github.com>
2023-04-13 23:33:29 +0000

    Slimmed down the cards.

--- procurement-manager/pages/orders/RequestCard.tsx
+++ procurement-manager/pages/orders/RequestCard.tsx
@@ -1,66 +1,30 @@
 // RequestCard.tsx
 import React from 'react';
-import { Col, Row, Card, Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 interface RequestCardProps {
   requestNumber: number;
   dateRequested: string;
   dateNeeded: string;
   orderTotal: number;
-  budgetUsed: string;
 }
 
-const RequestCard: React.FC<RequestCardProps> = ({
-  requestNumber,
-  dateRequested,
-  dateNeeded,
-  orderTotal,
-  budgetUsed,
-}) => {
+const RequestCard: React.FC<RequestCardProps> = ({ requestNumber, dateRequested, dateNeeded, orderTotal }) => {
   return (
-    <Card style={{ backgroundColor: '#f8f9fa' }}>
-      <Card.Body>
-        <Row className="smaller-row">
-          <Col>
-            <h4>Request Number</h4>
-            <h5>#{requestNumber}</h5>
-          </Col>
-          <Col>
-            <h4>Date Requested</h4>
-            <h5>{dateRequested}</h5>
-          </Col>
-          <Col>
-            <h4>Date Needed</h4>
-            <h5>{dateNeeded}</h5>
-          </Col>
-          <Col>
-            <h5>Order Total: ${orderTotal}</h5>
-            <h5>Budget Used: {budgetUsed}</h5>
-          </Col>
-        </Row>
-        <Row className="smaller-row d-flex justify-content-between">
-          <Col md="auto">
-            <a href="#" className="text-info" style={{ textDecoration: 'underline' }}>
-              View Request Form
-            </a>
-          </Col>
-          <Col md="auto">
-            <Button
-              variant="success"
-              size="lg"
-              style={{ minWidth: '300px', marginRight: '40px' }}
-            >
-              APPROVE
-            </Button>{' '}
-            <Button
-              variant="dark"
-              size="lg"
-              style={{ minWidth: '300px' }}
-            >
-              REJECT
-            </Button>{' '}
-          </Col>
-        </Row>
+    <Card className="mb-2">
+      <Card.Body className="d-flex justify-content-between align-items-center">
+        <a href="#" className="text-dark">
+          Request {requestNumber}
+        </a>
+        <span>Order Total: ${orderTotal}</span>
+        <span>Date Requested: {dateRequested}</span>
+        <span>Date Needed: {dateNeeded}</span>
+        <Button variant="success" size="sm">
+          Approve
+        </Button>
+        <Button variant="dark" size="sm">
+          Reject
+        </Button>
       </Card.Body>
     </Card>
   );
