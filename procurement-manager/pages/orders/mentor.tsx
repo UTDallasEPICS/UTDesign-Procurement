749c7da08e1841a34a1ed51aca17f4ed55bbb574
bryceabraham <121129581+bryceabraham@users.noreply.github.com>
2023-04-13 07:47:58 +0000

    Added show/hide button for the order of a project

--- procurement-manager/pages/orders/mentor.tsx
+++ procurement-manager/pages/orders/mentor.tsx
@@ -1,62 +1,87 @@
-import React from 'react'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Button, Collapse } from 'react-bootstrap';
 import TopBarComponent from './TopBarComponent';
 
-export default function mentor() {
+export default function Mentor() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <TopBarComponent />
-    <Container>
-        <Row className='big-row'>
-            <Row className='small-row'>
-                <Col Lg={10}>
-                  <h3>Project 2: Point of Nerve Conduction Diagnostic | Capstone, Request</h3>
-                </Col>
-                <Col Lg={2}>
-                  <h4>Budget: $100/500</h4>
-                </Col>
-            </Row>
-
-            <Row className='small-row'>
-              <Card>
-                <Card.Body>
-                  <Row className='smaller-row'>
-                    <Col>
-                      <h4>Request Number</h4>
-                      <h5>#1</h5>
-                    </Col>
-                    <Col>
-                    <h4>Date Requested</h4>
-                    <h5>4/11/2023</h5>
-                    </Col>
-                    <Col>
-                    <h4>Date Needed</h4>
-                    <h5>4/13/2023</h5>
-                    </Col>
-                    <Col>
-                    <h5>Order Total: $200</h5>
-                    <h5>Budget Used: $300/500</h5>
-                    </Col>
-                  </Row>
-                  <Row className='smaller-row'>
-                    <Col>
-                      <Button variant="danger" size="lg">REJECT</Button>{' '}
-                      <Button variant="success" size="lg">APPROVE</Button>{' '}
-                    </Col>
-                    <Col>
-                      <Button variant="dark" size="lg">REJECT</Button>{' '}
-                      <Button variant="success" size="lg">APPROVE</Button>{' '}
-                    </Col>
-
-                  </Row>
-
-                </Card.Body>
-
-              </Card>
-
-            </Row>
+      <TopBarComponent />
+      <Container>
+        <Row className="big-row">
+          <Row className="small-row">
+            <Col lg={10}>
+              <h3>
+                Project 2: Point of Nerve Conduction Diagnostic | Capstone,
+                Request
+              </h3>
+            </Col>
+            <Col lg={2}>
+              <h4>Budget: $100/500</h4>
+              <Button
+                variant="outline-secondary"
+                size="sm"
+                onClick={toggleCollapse}
+                aria-controls="collapseContent"
+                aria-expanded={isOpen}
+              >
+                {isOpen ? 'Hide' : 'Show'}
+              </Button>
+            </Col>
+          </Row>
+          <Collapse in={isOpen}>
+            <div id="collapseContent">
+              <Row className="small-row">
+                <Card>
+                  <Card.Body>
+                    <Row className="smaller-row">
+                      <Col>
+                        <h4>Request Number</h4>
+                        <h5>#1</h5>
+                      </Col>
+                      <Col>
+                        <h4>Date Requested</h4>
+                        <h5>4/11/2023</h5>
+                      </Col>
+                      <Col>
+                        <h4>Date Needed</h4>
+                        <h5>4/13/2023</h5>
+                      </Col>
+                      <Col>
+                        <h5>Order Total: $200</h5>
+                        <h5>Budget Used: $300/500</h5>
+                      </Col>
+                    </Row>
+                    <Row className="smaller-row">
+                      <Col>
+                        <Button variant="danger" size="lg">
+                          REJECT
+                        </Button>{' '}
+                        <Button variant="success" size="lg">
+                          APPROVE
+                        </Button>{' '}
+                      </Col>
+                      <Col>
+                        <Button variant="dark" size="lg">
+                          REJECT
+                        </Button>{' '}
+                        <Button variant="success" size="lg">
+                          APPROVE
+                        </Button>{' '}
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Row>
+            </div>
+          </Collapse>
         </Row>
-    </Container>
+      </Container>
     </>
-  )
+  );
 }
