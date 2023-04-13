f67264facae6485831691c3f1edb9f9efe1cad21
bryceabraham <121129581+bryceabraham@users.noreply.github.com>
2023-04-13 23:39:20 +0000

    making card prettier

--- procurement-manager/pages/orders/RequestCard.tsx
+++ procurement-manager/pages/orders/RequestCard.tsx
@@ -11,18 +11,18 @@ interface RequestCardProps {
 
 const RequestCard: React.FC<RequestCardProps> = ({ requestNumber, dateRequested, dateNeeded, orderTotal }) => {
   return (
-    <Card className="mb-2">
+    <Card className="mb-2" style={{ backgroundColor: '#f8f9fa' }}>
       <Card.Body className="d-flex justify-content-between align-items-center">
-        <a href="#" className="text-dark">
-          Request {requestNumber}
+        <a href="#" className="text-primary font-weight-bold">
+          Request #{requestNumber}
         </a>
-        <span>Order Total: ${orderTotal}</span>
-        <span>Date Requested: {dateRequested}</span>
-        <span>Date Needed: {dateNeeded}</span>
+        <span className="font-weight-bold mr-2">Order Total: ${orderTotal}</span>
+        <span className="font-weight-bold mr-2">Date Requested: {dateRequested}</span>
+        <span className="font-weight-bold mr-2">Date Needed: {dateNeeded}</span>
         <Button variant="success" size="sm">
           Approve
         </Button>
-        <Button variant="dark" size="sm">
+        <Button variant="danger" size="sm">
           Reject
         </Button>
       </Card.Body>
