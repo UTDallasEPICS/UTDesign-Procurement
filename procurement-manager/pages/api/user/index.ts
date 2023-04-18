ecaaae9ad5120d04c7d1828a99087ab071abd361
Razer323 <razer32323@gmail.com>
2023-04-18 15:36:03 -0500

    Updating branch

--- procurement-manager/pages/api/user/index.ts
+++ procurement-manager/pages/api/user/index.ts
@@ -1,4 +1,6 @@
+import { Prisma } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/db'
 // POST request
 // get email, first name, last name from SSO,
 // get netID from email
@@ -21,15 +23,56 @@ import { NextApiRequest, NextApiResponse } from 'next'
  * Type safe for requesting body
  * https://stackoverflow.com/questions/69893369/how-to-add-typescript-types-to-request-body-in-next-js-api-route/70788003#70788003
  */
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
+  
 ) {
-  const { body } = req
+    await prisma.role.createMany({
+      data:[{
+        role : 'Admin'
+      },
+      {
+        role: 'Mentor'
+      },
+      {
+        role: 'Student'
+      }
+        
+      ]
+    })
+    await prisma.user.create({
+      data : {
+        netID: "AXT210117",
+        firstName: 'Ananth',
+        lastName: 'Tekkalakota',
+        email: 'axt210117@utdallas.edu',
+        active : true , 
+        role : {connect: {roleID: 1}}
+      }})
+
+  if(req.method == 'GET'){
+    const{Firstname, Lastname, email } = req.body;
+    const user = prisma.user.findUnique({
+      where:{
+          //email.slice(0,9)
+          email,
+      },
+    });
+    if(!user){
+      res.status(404).json({error: 'User Not Found'})
+      return;
+    }else {
+      res.status(200).json({user: user});
+    }
+  } 
+  }
+  /*const { body } = req
   try {
     if ('role' in body && typeof body.role === 'string') {
       const role: string = req.body.role
       res.status(200).json({ givenRole: role })
     }
   } catch (error) {}
-}
+}*/
