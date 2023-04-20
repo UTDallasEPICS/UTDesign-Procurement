0f686d8255d3f62b54784dbc71ea7b4526ea2b5d
bryceabraham <121129581+bryceabraham@users.noreply.github.com>
2023-04-20 17:10:39 +0000

    Updates to Request Form

--- procurement-manager/pages/orders/studentrequest.tsx
+++ procurement-manager/pages/orders/studentrequest.tsx
@@ -8,6 +8,10 @@ const StudentRequest = () => {
   const [quantity, setQuantity] = useState("");
   const [unitCost, setUnitCost] = useState("");
   const [totalCost, setTotalCost] = useState("");
+  const [expenseJustification, setExpenseJustification] = useState("");
+  const [additionalInfo, setAdditionalInfo] = useState("");
+  const [itemDescription, setItemDescription] = useState("");
+  const [itemLink, setItemLink] = useState("");
 
   const handleVendorChange = (e: ChangeEvent<HTMLInputElement>) => {
     setVendor(e.target.value);
@@ -33,6 +37,24 @@ const StudentRequest = () => {
     setTotalCost(e.target.value);
   };
 
+   const handleExpenseJustificationChange = (
+    e: ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setExpenseJustification(e.target.value);
+  };
+
+  const handleAdditionalInfoChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setAdditionalInfo(e.target.value);
+  };
+
+  const handleItemDescriptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setItemDescription(e.target.value);
+  };
+
+  const handleItemLinkChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setItemLink(e.target.value);
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted!");
@@ -42,7 +64,7 @@ const StudentRequest = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h1 className={styles.requestForm}>New Purchase Request</h1>
+        <h1 className={styles.requestForm}>Request Form</h1>
       </div>
       <div className={styles.budgetInfo}>
         <div className={styles.budget}>
@@ -119,6 +141,56 @@ const StudentRequest = () => {
           id="totalCost"
           value={totalCost}
           onChange={handleTotalCostChange}
+        />
+         <label className={styles.expenseJustification} htmlFor="expenseJustification">
+          Expense Justification
+        </label>
+        <textarea
+          className={styles.expenseField}
+          id="expenseJustification"
+          value={expenseJustification}
+          onChange={handleExpenseJustificationChange}
+        ></textarea>
+
+        <label className={styles.additionalInfo} htmlFor="additionalInfo">
+          Additional Information
+        </label>
+        <textarea
+          className={styles.additionalinfoField}
+          id="additionalInfo"
+          value={additionalInfo}
+          onChange={handleAdditionalInfoChange}
+        ></textarea>
+
+        <label className={styles.itemDescription} htmlFor="itemDescription">
+          Item Description
+        </label>
+        <input
+          className={styles.itemdescriptionField}
+          type="text"
+          id="itemDescription"
+          value={itemDescription}
+          onChange={handleItemDescriptionChange}
+        />
+
+        <label className={styles.itemLink} htmlFor="itemLink">
+          Item Link
+        </label>
+        <input
+          className={styles.itemlinkField}
+          type="text"
+          id="itemLink"
+          value={itemLink}
+          onChange={handleItemLinkChange}
+        />
+
+        <label className={styles.supportingDocs} htmlFor="supportingDocs">
+          Supporting Documents
+        </label>
+        <input
+          className={styles.supportingdocsField}
+          type="file"
+          id="supportingDocs"
         />
                 <button className={styles.submitButton} type="submit">
           Submit
