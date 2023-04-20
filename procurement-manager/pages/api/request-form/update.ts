e16c28cef70fd83ae17c6368b2ba5b23ba70025e
Isaac Baldivas <ice.baldivas@gmail.com>
2023-04-20 12:38:35 -0500

    request-form api -- loose update function

---
+++ procurement-manager/pages/api/request-form/update.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/db'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // TODO:: Add validation
+  // TODO:: Add error handling
+  // TODO:: Type safety
+  // Right now, if there is anything in the req.body, then it will be updated
+  try {
+    const request = await prisma.request.update({
+      where: {
+        requestID: req.body.requestID,
+      },
+      data: {
+        // Updating Request
+        dateOrdered: req.body.dateOrdered,
+        dateReceived: req.body.dateReceived,
+        dateApproved: req.body.dateApproved,
+        // fix where if there is req.body.justification or additionalInfo is null, just keep what it was before
+        justification: req.body.justification,
+        additionalInfo: req.body.additionalInfo,
+        Order: {
+          updateMany: {
+            where: { requestID: req.body.requestID },
+            data: {
+              dateOrdered: req.body.dateOrdered,
+              orderNumber: req.body.orderNumber,
+              orderDetails: req.body.orderDetails,
+              trackingInfo: req.body.trackingInfo,
+              shippingCost: req.body.shippingCost,
+            },
+          },
+        },
+        // Updating Request Items
+        RequestItem: {
+          updateMany: {
+            where: {
+              requestID: req.body.requestID,
+            },
+            data: {
+              itemID: req.body.itemID,
+              quantity: req.body.quantity,
+              description: req.body.description,
+            },
+          },
+        },
+        // Updating Other Expenses
+        OtherExpense: {
+          updateMany: {
+            where: {
+              requestID: req.body.requestID,
+            },
+            data: {
+              expenseID: req.body.expenseID,
+              expenseAmount: req.body.expenseAmount,
+              expenseComments: req.body.expenseComments,
+            },
+          },
+        },
+      },
+    })
+    res.status(200).json({ message: 'Request Form Updated', request: request })
+  } catch (err) {
+    if (err instanceof Error)
+      res
+        .status(500)
+        .json({ message: 'Internal Server Error', error: err.message })
+    else res.status(500).json({ message: 'Internal Server Error', error: err })
+  }
+}
