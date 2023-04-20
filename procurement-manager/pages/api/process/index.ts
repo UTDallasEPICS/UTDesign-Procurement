2c16109be7954ace5ee72a353d317a65812f5aaa
NadeebaAtiqui <123310283+NadeebaAtiqui@users.noreply.github.com>
2023-04-20 14:20:10 -0500

    process api initial

--- procurement-manager/pages/api/process/index.ts
+++ procurement-manager/pages/api/process/index.ts
@@ -1,4 +1,4 @@
-//Menotr will either approve or reject request for tiem and provide a comment as to why
+//Menotr will either approve or reject request for item and provide a comment as to why
 //this information needs to be sent to the student (status update)
 //only approved request by mentor reaches admin
 //admin approves or declines this information is then sent to student and mentor withe comments
@@ -19,4 +19,39 @@
 // IF PROCESS IS APPROVED BY ADMIN
 // create an order and put in database
 // ??? ask Oddrun and Navaneeth about how creating a new Order into database will work
-export default function handler() {}
+
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+      // Handle GET request
+      // Fetch all processes from the Prisma model
+      const processes = await prisma.process.findMany();
+      res.status(200).json(processes);
+    } else if (req.method === 'POST') {
+      // Handle POST request
+      const { status, mentorProcessed, mentorProcessedComments, adminProcessed, adminProcessedComments, mentorID, adminID, requestID, reimbursementID } = req.body;
+      // Create a new process in the Prisma model
+      const newProcess = await prisma.process.create({
+        data: {
+          status,
+          mentorProcessed: new Date(mentorProcessed),
+          mentorProcessedComments,
+          adminProcessed: new Date(adminProcessed),
+          adminProcessedComments,
+          mentorID,
+          adminID,
+          requestID,
+          reimbursementID
+        }
+      });
+      res.status(201).json(newProcess);
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' });
+    }
+  }
+  export { prisma };
+  
+
