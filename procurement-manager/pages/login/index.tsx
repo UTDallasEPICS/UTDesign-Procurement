b4dc3881cb346449b6e285687a147a55d8f71447
Fred248 <143210606+Fred248@users.noreply.github.com>
2023-09-27 16:42:03 -0500

    Update Index.tsx

--- procurement-manager/pages/login/index.tsx
+++ procurement-manager/pages/login/index.tsx
@@ -48,7 +48,7 @@ export default function Login() {
                     />
                   </Form.Group>
                   <Button
-                    style={{width:210, backgroundColor: "dark green", marginTop:20, borderColor: "dark green"}}
+                    style={{width: 210, backgroundColor:'darkgreen', marginTop:20, borderColor: "black"}}
                     variant='primary' onClick={handleLogin}>
                     Login
                   </Button>
