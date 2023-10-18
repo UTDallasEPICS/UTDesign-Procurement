93895671229be192be16e30d0654d5fb34ab37be
Fred248 <143210606+Fred248@users.noreply.github.com>
2023-10-18 15:02:58 -0500

    Update admin.tsx

--- procurement-manager/pages/projects/admin.tsx
+++ procurement-manager/pages/projects/admin.tsx
@@ -11,18 +11,20 @@ const Ready = ({ title }: { title: string }) => {
       <Head>
         <title> {title} </title>
       </Head>
-      <div style={{ 
+      <Button 
+        style={{ 
         position: 'absolute', 
         width: 1350, height: 100, 
-        top: 150, left: 85, 
+        top: 150, left: 85,
+        borderColor: 'black', 
         backgroundColor: 'lightgray' }}>
-        <p style = {{position: 'absolute', fontSize: 25, textIndent: 20}}>#</p>
-        <p style = {{position: 'absolute', fontSize: 25, color: 'green', textIndent: 125}}>Template Project</p>
-        <p style = {{position: 'absolute', fontSize: 25, textIndent: 700}}>Total Budget:</p>
-        <p style = {{position: 'absolute', fontSize: 25, textIndent: 1000}}>Remaining Budget:</p>
-        <p style = {{position: 'absolute', fontSize: 16, textIndent: 20, bottom: 20}}>Mentors:</p>
-        <p style = {{position: 'absolute', fontSize: 16, textIndent: 20, bottom: -5}}>Students:</p>
-      </div>
+        <p style = {{position: 'absolute', fontSize: 25, color: 'black', textIndent: 20, bottom: 45}}>#</p>
+        <p style = {{position: 'absolute', fontSize: 25, color: 'green', textIndent: 125, bottom: 45}}>Template Project</p>
+        <p style = {{position: 'absolute', fontSize: 25, color: 'black', textIndent: 700, bottom: 45}}>Total Budget:</p>
+        <p style = {{position: 'absolute', fontSize: 25, color: 'black', textIndent: 1000, bottom: 45}}>Remaining Budget:</p>
+        <p style = {{position: 'absolute', fontSize: 16, color: 'black', textIndent: 20, bottom: 20}}>Mentors:</p>
+        <p style = {{position: 'absolute', fontSize: 16, color: 'black', textIndent: 20, bottom: -5}}>Students:</p>
+      </Button>
       
     </>
   );
