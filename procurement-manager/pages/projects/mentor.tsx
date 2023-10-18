53eaf1cbfc797dc5f4a8592ad039deb5496aaf75
anwitagudapuri <143034795+anwitagudapuri@users.noreply.github.com>
2023-10-18 15:07:07 -0500

    Create mentor.tsx

---
+++ procurement-manager/pages/projects/mentor.tsx
@@ -0,0 +1,18 @@
+/**
+ * This is the Mentor View for the Projects Page
+ */
+import React from 'react';
+import Head from 'next/head';
+
+const NotReadyMessage = ({ title }: { title: string }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div>Not ready yet</div>
+    </>
+  );
+};
+
+export default NotReadyMessage;
