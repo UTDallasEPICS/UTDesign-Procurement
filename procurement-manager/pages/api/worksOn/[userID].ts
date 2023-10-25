8b09d4da2aa6de0ea0ef27176c876d347fcd0587
ElenaP268 <paulelena04@gmail.com>
2023-10-25 01:33:14 -0500

    added PUT API that updates a user's worksOn based on input userID and projectID

--- procurement-manager/pages/api/worksOn/[userID].ts
+++ procurement-manager/pages/api/worksOn/[userID].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@/db'
+import axios from 'axios';
 
 export default async function handler(
   req: NextApiRequest,
@@ -28,5 +29,59 @@ export default async function handler(
         catch (error) {
             res.status(500).json({error: 'WorksOn not found'})
         }
+    }
+    else if (req.method === 'PUT') {
+        try{
+            const {
+                query: {userID, projectID},
+                method,
+            } = req;
+            // request header (since 2 query params): /api/WorksOn/*userID*/?projectID=*projectID*
+            // replace ** with the values
+
+            // PUT will update a user's worksOn  based on passed in userID and projectID
+            // this happens by deleting the user's current worksOn if needed and then creating a new worksOn (so now only works if user has just 0 or 1 worksOn)
+            const project = await prisma.project.findUnique({
+                where:
+                { projectID: Number(projectID)},
+            })
+            if (!project) throw new Error('Project not found')
+
+            const user = await prisma.user.findUnique({
+                where:
+                { userID: Number(userID)},
+            })
+            if (!user) throw new Error('User not found')
+
+            const worksOnCur = await prisma.worksOn.findFirst({
+                where: {
+                    userID: Number(userID),
+                }
+            })
+
+            if (worksOnCur !== null) {
+                const worksOnDeleted = await prisma.worksOn.delete({
+                    where: {
+                        userID_projectID: {userID: user.userID, projectID: worksOnCur.projectID},
+                    },
+                })
+            }
+
+            const worksOnNew = await prisma.worksOn.create({
+                data: {
+                    user: { connect: { userID: user.userID } },
+                    project: { connect: { projectID: project.projectID } }
+                }
+            })
+
+            res.status(200).json({ worksOnNew })
+        }
+        catch (error) {
+            if (error instanceof Error) res.status(500).json({message: error.message})
+        }
+    }
+    else
+    {
+        res.status(400).json({ error: 'Invalid method' })
     }
 }
