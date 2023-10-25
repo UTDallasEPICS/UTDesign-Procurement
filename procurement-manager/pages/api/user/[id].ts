985af2b402910a46cae1bd1dbfa8e16e360bb049
ElenaP268 <paulelena04@gmail.com>
2023-10-25 00:21:54 -0500

    added updated POST API to get all users in a project using projectID

--- procurement-manager/pages/api/user/[id].ts
+++ procurement-manager/pages/api/user/[id].ts
@@ -1,3 +1,8 @@
+/*
+if get request then returns user for userID query param
+if post request then returns all users in a project for projectID query param
+*/
+
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@/db'
 
@@ -7,16 +12,34 @@ export default async function handler(
 ) {
   try {
     const id = parseInt(req.query.id as string)
-    const user = await prisma.user.findUnique({
-      where: {
-        userID: id,
-      },
-    })
-    if (!user) {
-      res.status(404).json({ message: 'User not found' })
-      throw new Error('Could not find that user')
+    if (req.method === 'GET') {
+      const user = await prisma.user.findUnique({
+        where: {
+          userID: id,
+        },
+      })
+      if (!user) {
+        res.status(404).json({ message: 'User not found' })
+        throw new Error('Could not find that user')
+      }
+      res.status(200).json(user)
     }
-    res.status(200).json(user)
+    else if (req.method == 'POST') {
+        const project = await prisma.project.findUnique({
+            where:
+            { projectID: id},
+            include:
+            { WorksOn: true }
+        })
+
+        if (!project) throw new Error('Project not found')
+
+        const users = await prisma.user.findMany({
+            where: { WorksOn: { some: { projectID: project?.projectID } } }, // find all users with this project in their WorksOn list
+        })
+
+        res.status(200).json({ users })
+    }
   } catch (error) {
     res.status(500).json(error)
   }
