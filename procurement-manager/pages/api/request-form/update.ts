c484af11b34a8c1b4b17f4e0352b4bc0441c1ffa
ElenaP268 <paulelena04@gmail.com>
2023-11-23 15:04:36 -0600

    fixed request form update API to save edited request info to DB

--- procurement-manager/pages/api/request-form/update.ts
+++ procurement-manager/pages/api/request-form/update.ts
@@ -1,80 +1,174 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '@/db'
+import { Decimal } from '@prisma/client/runtime'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // TODO:: Add validation
-  // TODO:: Add error handling
-  // TODO:: Type safety
-  // Right now, if there is anything in the req.body, then it will be updated
   try {
-    const { requestID, requestDetails } = req.body
-    console.log(requestDetails.RequestItem)
+    const body = req.body
+    let request
+
     const oldRequestForm = await prisma.request.findUnique({
       where: {
-        requestID: requestID,
+        requestID: parseInt(body.requestID),
       },
     })
     if (!oldRequestForm) throw new Error('Could not find that request form')
-    const request = await prisma.request.update({
+
+    if ('orderID' in body) { // order details - optional, so only updated if passed in
+
+      let order = await prisma.order.findUnique({
+        where: {
+          orderID: parseInt(body.orderID)
+        }
+      })
+      if (!order) throw new Error('Could not find order')
+
+      if ('orderNumber' in body) {
+        request = await prisma.request.update({
+          where: {
+            requestID: oldRequestForm.requestID
+          },
+          data: {
+            Order: {
+              update: {
+                where: {
+                  orderID: parseInt(body.orderID)
+                },
+                data: {
+                  orderNumber: String(body.orderNumber),
+                }
+              }
+            }
+          }
+        })
+      }
+      if ('trackingInfo' in body) {
+        request = await prisma.request.update({
+          where: {
+            requestID: oldRequestForm.requestID
+          },
+          data: {
+            Order: {
+              update: {
+                where: {
+                  orderID: parseInt(body.orderID)
+                },
+                data: {
+                  trackingInfo: String(body.trackingInfo),
+                }
+              }
+            }
+          }
+        })
+      }
+      if ('shippingCost' in body) {
+        request = await prisma.request.update({
+          where: {
+            requestID: oldRequestForm.requestID
+          },
+          data: {
+            Order: {
+              update: {
+                where: {
+                  orderID: parseInt(body.orderID)
+                },
+                data: {
+                  shippingCost: new Decimal(body.shippingCost)
+                }
+              }
+            }
+          }
+        })
+      }
+    }
+    if ('processID' in body) { 
+      let process = await prisma.process.findUnique({
+        where: {
+          processID: parseInt(body.processID)
+        }
+      })
+      if (!process) throw new Error('Could not find process')
+      
+      if ('status' in body) { // status - optional
+        request = await prisma.request.update({
+          where: {
+            requestID: oldRequestForm.requestID,
+          },
+           data: {
+             Process: {
+              update: {
+                where: {
+                  processID: parseInt(body.processID)
+                },
+                data: {
+                  status: body.status
+                }
+              }
+             }
+           },
+        })
+      }
+    }
+    let requestItem = await prisma.requestItem.findUnique({
       where: {
-        requestID: oldRequestForm.requestID,
-      },
-      data: {
-        ...requestDetails,
-      },
-      // data: {
-      //   // Updating Request
-      //   dateOrdered: req.body.dateOrdered,
-      //   dateReceived: req.body.dateReceived,
-      //   dateApproved: req.body.dateApproved,
-      //   // fix where if there is req.body.justification or additionalInfo is null, just keep what it was before
-      //   justification: req.body.justification,
-      //   additionalInfo: req.body.additionalInfo,
-      //   Order: {
-      //     updateMany: {
-      //       where: { requestID: req.body.requestID },
-      //       data: {
-      //         dateOrdered: req.body.dateOrdered,
-      //         orderNumber: req.body.orderNumber,
-      //         orderDetails: req.body.orderDetails,
-      //         trackingInfo: req.body.trackingInfo,
-      //         shippingCost: req.body.shippingCost,
-      //       },
-      //     },
-      //   },
-      //   // Updating Request Items
-      //   RequestItem: {
-      //     updateMany: {
-      //       where: {
-      //         requestID: req.body.requestID,
-      //       },
-      //       data: {
-      //         itemID: req.body.itemID,
-      //         quantity: req.body.quantity,
-      //         description: req.body.description,
-      //       },
-      //     },
-      //   },
-      //   // Updating Other Expenses
-      //   OtherExpense: {
-      //     updateMany: {
-      //       where: {
-      //         requestID: req.body.requestID,
-      //       },
-      //       data: {
-      //         expenseID: req.body.expenseID,
-      //         expenseAmount: req.body.expenseAmount,
-      //         expenseComments: req.body.expenseComments,
-      //       },
-      //     },
-      //   },
-      // },
+        itemID: parseInt(body.itemID)
+      }
     })
+    if (!requestItem) throw new Error('Could not find request item')
+
+    if ('vendorID' in body) { // vendor details - optional
+      request = await prisma.request.update({
+        where: {
+          requestID: oldRequestForm.requestID,
+        },
+         data: {
+           RequestItem: {
+             update: {
+               where: {
+                 itemID: parseInt(body.itemID),
+               },
+               data: {
+                 description: String(body.description),
+                 url: String(body.url),
+                 partNumber: String(body.partNumber),
+                 quantity: parseInt(body.quantity),
+                 unitPrice: new Decimal(body.unitPrice),
+                 vendorID: parseInt(body.vendorID)
+               },
+             },
+           },
+         },
+      })
+    } 
+    else {
+      request = await prisma.request.update({ // update with default parameters
+        where: {
+          requestID: oldRequestForm.requestID,
+        },
+         data: {
+           RequestItem: {
+             update: {
+               where: {
+                 itemID: parseInt(body.itemID),
+               },
+               data: {
+                 description: String(body.description),
+                 url: String(body.url),
+                 partNumber: String(body.partNumber),
+                 quantity: parseInt(body.quantity),
+                 unitPrice: new Decimal(body.unitPrice)
+               },
+             },
+           },
+         },
+      })
+    }
     res.status(200).json({ message: 'Request Form Updated', request: request })
   } catch (err) {
+    console.log(err)
     if (err instanceof Error)
       res
         .status(500)
