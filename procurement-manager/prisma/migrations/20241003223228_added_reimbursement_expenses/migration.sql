dd57f6f4d051d919bc4fba6bcb42b190a221f9c3
Colin Wong <cwongmath@gmail.com>
2024-11-21 15:55:55 -0600

    re-add migration properly

--- procurement-manager/prisma/migrations/20241003223228_added_reimbursement_expenses/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `reimbursement` ADD COLUMN `expense` DECIMAL(65, 30) NOT NULL DEFAULT 0;
---
+++ procurement-manager/prisma/migrations/20241121215538_add_reimbursement_expense/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Reimbursement` ADD COLUMN `expense` DECIMAL(65, 30) NOT NULL DEFAULT 0;
