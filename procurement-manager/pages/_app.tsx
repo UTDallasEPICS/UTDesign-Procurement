bdcfaf9524640bd7cd386a3b1e8d39b3ef337ab3
Jason Antwi-Appah <hey@jasonaa.me>
2025-05-08 12:54:19 -0500

    Remove unneeded dependency

--- procurement-manager/pages/_app.tsx
+++ procurement-manager/pages/_app.tsx
@@ -3,7 +3,6 @@ import { SSRProvider } from 'react-bootstrap'
 import Layout from '@/components/Layout'
 import { SessionProvider } from 'next-auth/react'
 import '@/styles/globals.scss'
-import { NuqsAdapter } from 'nuqs/adapters/next/pages'
 
 export default function App({
   Component,
@@ -13,9 +12,7 @@ export default function App({
     <SSRProvider>
       <SessionProvider session={session}>
         <Layout>
-          <NuqsAdapter>
-            <Component {...pageProps} />
-          </NuqsAdapter>
+          <Component {...pageProps} />
         </Layout>
       </SessionProvider>
     </SSRProvider>
